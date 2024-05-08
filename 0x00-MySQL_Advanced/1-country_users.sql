c18b0a1ac7d1d2f776e0cc03d4d1671b93066bf9
aladaabdul <aladarahman18@gmail.com>
2024-05-08 23:08:25 +0100

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users(
 	id INT AUTO_INCREMENT NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	name VARCHAR(255),
-	country SET('US', 'CO', 'TN') DEFAULT 'US'
+	country ENUM('US', 'CO', 'TN') NOT NULL	 DEFAULT 'US'
 	PRIMARY KEY (id),
 	UNIQUE (email)
 	);
