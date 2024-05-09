4407c584d5eaf40be74dc99091832fa72237112a
aladaabdul <aladarahman18@gmail.com>
2024-05-09 14:06:00 +0100

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,11 +1,12 @@
 -- Create table users
 
-DROP TABLE IF NOT EXISTS users;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id INT AUTO_INCREMENT NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	name VARCHAR(255),
-	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+	country CHAR(2) NOT NULL DEFAULT 'US',
+	CHECK (country IN ('US', 'CO', 'TN')),
 	PRIMARY KEY (id),
 	UNIQUE (email)
 	);
