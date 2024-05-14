f3bc8aa65cdd35e6d74e9caeab41e3365cc0468c
aladaabdul <aladarahman18@gmail.com>
2024-05-14 18:12:08 +0100

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+""" function that changes all topics of a school document based on the name"""
+
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document based on the name
+
+    """
+    value = {"school": name}
+    updated_value = {"$set": {"topics": topics}}
+    result = mongo_collection.update_one(value, updated_value)
+    return result
