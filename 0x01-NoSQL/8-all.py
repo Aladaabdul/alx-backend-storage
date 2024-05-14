ba97ca6b59a48ba75c33dae50da8c93b2abc902d
aladaabdul <aladarahman18@gmail.com>
2024-05-14 14:32:58 +0100

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+"""function that lists all documents in a collection"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+
+    """List all doc in a collection
+    """
+    if mongo_collection is None:
+        return []
+    return [doc for doc in mongo_collection.find()]
