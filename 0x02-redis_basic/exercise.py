43e0f556bca95a1de257d055e94ea81a10681884
aladaabdul <aladarahman18@gmail.com>
2024-05-15 19:51:29 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,25 @@
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of times a method is called.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function that increments the count and calls the original method.
+        """
+        # Generate the key using the method's qualified name
+        key = f"{method.__qualname__}_calls"
+        # Increment the count in Redis
+        self._redis.incr(key)
+        # Call the original method
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     """Class Cache implementation
@@ -20,6 +38,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method that takes a data argument
         and return a string
@@ -38,7 +57,7 @@ class Cache:
         """get method that take a key string argument
         and an optional Callable argument named fn
         """
-        data = self._radis.get(key)
+        data = self._redis.get(key)
         if data is None:
             return None
         if fn:
